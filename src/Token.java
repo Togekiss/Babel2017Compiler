0c4fd3801e7eca06638a4c4befa95a513e32136d
Marta "Togekiss" Zapatero <martazapateroweb@gmail.com>
2017-02-14 18:05:34 +0100

    Classe Token creada

---
+++ src/Token.java
@@ -0,0 +1,22 @@
+
+public class Token {
+	
+	private String tipus;
+	private String lexema;
+	
+	public String getTipus() {
+		return tipus;
+	}
+	public void setTipus(String tipus) {
+		this.tipus = tipus;
+	}
+	public String getLexema() {
+		return lexema;
+	}
+	public void setLexema(String lexema) {
+		this.lexema = lexema;
+	}
+	
+	
+
+}
