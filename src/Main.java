a7de0c3acd3389eeecf4f66bc5ab734abc19d6d3
rmarr_000 <rmarr_000@172.20.56.178>
2017-02-21 18:18:13 +0100

    Fitxer error

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,20 @@
 public class Main {
 	
 	public static void main(String[] args) {
+		String name = new String();
+		char c;
+		int i = 0;
+		
+		c = args[0].charAt(i);
+		while (c != '.') { 
+			name += c;
+			c = args[0].charAt(++i);
+		}
 		
 		System.out.println("Imprimint tokens del fitxer " + args[0]);
 		
 		Alex alex = new Alex(args[0]);
+		Error error = new Error(name);
 		
 		Token token = new Token("null", "null");
 		
@@ -13,5 +23,6 @@ public class Main {
 			token = alex.getToken();
 			System.out.println("<" + token.getTipus() + ", " + token.getLexema() + ">");
 		}
+		error.tancaFitxer();
 	}
 }
