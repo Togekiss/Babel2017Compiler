ce7f9014142d7715c9cb089c5e5f5fa40340ab93
rmarr_000 <rmarr_000@172.20.25.33>
2017-03-14 17:47:50 +0100

    Classe Excepció

--- src/analitzadorSintactic/SyntacticError.java
+++ src/analitzadorSintactic/SyntacticError.java
@@ -1,5 +1,14 @@
 package analitzadorSintactic;
 
+@SuppressWarnings("serial")
 public class SyntacticError extends Exception{
 
+	public SyntacticError() { super(); }
+	
+	public SyntacticError(String message) { super(message); }
+	
+	public SyntacticError(String message, Throwable cause) { super(message, cause); }
+	
+	public SyntacticError(Throwable cause) { super(cause); }
+	
 }
